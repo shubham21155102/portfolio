edf7ddea13e555541ee05688c74a3b537f7fb0eb
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-12-22 00:48:08 +0530

    content added

--- apps/web/lib/mdx.tsx
+++ apps/web/lib/mdx.tsx
@@ -1,22 +1,9 @@
 import fs from 'fs';
-import matter from 'gray-matter'
 const getBlogsMetadata=()=>{
     const folderPath="blogs/";
     const files=fs.readdirSync(folderPath);
     const markdownFiles=files.filter((file)=>file.endsWith(".md"));
-    // const slugs=markdownFiles.map((file)=>file.replace(".md",""));
-    // return slugs;
-    const blogs=markdownFiles.map((fileName)=>{
-        const filePath=`${folderPath}${fileName}.md`;
-        const fileContents=fs.readFileSync(filePath,'utf8');
-        const matterResult=matter(fileContents);
-        return {
-            title:matterResult.data.title,
-            date:matterResult.data.date,
-            tags:matterResult.data.tags,
-            summary:matterResult.data.summary,
-            slug:fileName.replace(".md","")
-        }
-    });
+    const slugs=markdownFiles.map((file)=>file.replace(".md",""));
+    return slugs;
 }
 export default getBlogsMetadata;
