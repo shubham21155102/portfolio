9d290dc1a9cdd3a1c7f0e65b52cd864b4b3ebdac
shubham <shubham.kumar.min21@itbhu.ac.in>
2023-12-24 11:04:26 +0530

    embed

--- apps/web/app/blog/page.tsx
+++ apps/web/app/blog/page.tsx
@@ -1,23 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
 import getBlogsMetadata from '../../lib/mdx'
 import Link from 'next/link'
+import dynamic from 'next/dynamic'
 type Props = {}
 
 const Blog = (props: Props) => {
-  const blogs = getBlogsMetadata()
-  console.log(blogs)
+  // const blogs = getBlogsMetadata()
+  // console.log(blogs)
+  useEffect(() => {
+    const screenWidth = window.screen.width;
+    const screenHeight = window.screen.height;
+    const embedContainer = document.getElementById('embedContainer');
+    if (embedContainer) {
+      embedContainer.style.width = `${screenWidth}px`;
+      embedContainer.style.height = `${screenHeight}px`;
+    }
+  }, []);
   return (
     <>
-      <h1>Blog</h1>
-      <ul>
+      <div id="embedContainer">
+        <embed src='https://learn.shubhamiitbhu.in/' width='100%' height='100%' />
+      </div>
+      {/* <h1>Blog</h1> */}
+      {/* <ul>
         {blogs.map((blog) => (
           <li key={blog}>
             <Link href={`/blog/${blog}`}>{blog}</Link>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </>
   )
 }
-
-export default Blog
+const BlogCSR = dynamic(() => Promise.resolve(Blog), { ssr: false })
+export default BlogCSR
+// export default Blog
