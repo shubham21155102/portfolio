669a1239909ebfdf389f6c4720c92c9b3c1a34cb
shubham <shubham.kumar.min21@itbhu.ac.in>
2024-02-06 18:23:50 +0530

    add resume

---
+++ apps/web/app/resume/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+   <>
+   {/* https://drive.google.com/file/d/1G7-YYaaL278Yz1nv5Jjo5fUtDDY5DGg1/preview
+ */}
+<iframe src="https://drive.google.com/file/d/1G7-YYaaL278Yz1nv5Jjo5fUtDDY5DGg1/preview" width="100%" height="1000px"></iframe>
+   </>
+  )
+}
+
+export default page
