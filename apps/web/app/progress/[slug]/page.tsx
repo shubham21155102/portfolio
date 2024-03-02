7123fcc12168bc89312215f82307f793f86ef561
shubham <shubham.kumar.min21@itbhu.ac.in>
2024-03-02 12:31:22 +0530

    add design little bit

--- apps/web/app/progress/[slug]/page.tsx
+++ apps/web/app/progress/[slug]/page.tsx
@@ -19,48 +19,61 @@ const IndividualTopics = (props: any) => {
                 <h2 className="text-lg font-bold text-black">{item.sl_no}</h2>
                 <p className="mt-2 text-gray-800">{item.title}</p>
                 <div className="mt-4">
-                  {item.topics.map((topic, index) => {
-                    return (
-                      <div key={index} className="relative bg-gray-100 rounded-lg shadow-md mb-4">
-                        <div className="p-4 flex items-center">
-                          <h3 className="text-lg font-bold text-black mr-2">{topic.sl_no_a2z}</h3>
-                          <p className="mt-2 text-gray-900 mr-2 px-10">{topic.title}</p>
-                          <Link href={`${topic.yt_link}`} passHref>
-                            <Image 
-                              src={"/youtube.svg"}
-                              alt="YouTube Link"
-                              width={35}
-                              height={35}
-                              className="rounded-full px-2 size-10"
-                            />
-                          </Link>
-                          <p className="mt-2 text-gray-900 mr-2 hidden absolute top-0 left-full bg-white p-2 shadow-lg rounded-lg border border-gray-200 pointer-events-none opacity-0 transition-opacity duration-200 group-hover:opacity-100 ">{topic.tags}</p>
-                          {topic.p1_link && (
-                            <Link href={`${topic.p1_link}`} target="__blank">
-                              <Image
-                                src={"/cn.svg"}
-                                alt="Link 1"
-                                width={50}
-                                height={100}
-                                className="rounded-full px-2 size-10"
-                              />
-                            </Link>
-                          )}
-                          {topic.p2_link && (
-                            <Link href={`${topic.p2_link}`} target="__blank">
-                              <Image
-                                src={"/leetcode.png"}
-                                alt="Link 2"
-                                width={25}
-                                height={25}
-                                className="rounded-full px-2 size-10"
-                              />
-                            </Link>
-                          )}
-                        </div>
-                      </div>
-                    );
-                  })}
+                {item.topics.map((topic, index) => (
+  <div key={index} className="relative bg-gray-100 rounded-lg shadow-md mb-4">
+    <table className="w-full">
+      <tbody>
+        <tr>
+          <td className="p-4 flex items-center">
+            <h3 className="text-lg font-bold text-black mr-2">{topic.sl_no_a2z}</h3>
+            <p className="mt-2 text-gray-900 mr-2 px-10">{topic.title}</p>
+          </td>
+          <td className="p-4">
+            <Link href={`${topic.yt_link}`} passHref>
+              <Image 
+                src={"/youtube.svg"}
+                alt="YouTube Link"
+                width={35}
+                height={35}
+                className="rounded-full"
+              />
+            </Link>
+          </td>
+          <td className="p-4">
+            <p className="mt-2 text-gray-900 mr-2 hidden absolute top-0 left-full bg-white p-2 shadow-lg rounded-lg border border-gray-200 pointer-events-none opacity-0 transition-opacity duration-200 group-hover:opacity-100 ">{topic.tags}</p>
+          </td>
+          <td className="p-4">
+            {topic.p1_link && (
+              <Link href={`${topic.p1_link}`} target="__blank">
+                <Image
+                  src={"/cn.svg"}
+                  alt="Link 1"
+                  width={100}
+                  height={100}
+                  className="rounded-full"
+                />
+              </Link>
+            )}
+          </td>
+          <td className="p-4">
+            {topic.p2_link && (
+              <Link href={`${topic.p2_link}`} target="__blank">
+                <Image
+                  src={"/leetcode.png"}
+                  alt="Link 2"
+                  width={25}
+                  height={25}
+                  className="rounded-full"
+                />
+              </Link>
+            )}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+))}
+
                 </div>
               </div>
             );
