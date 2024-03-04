47dbb3195050495077679ac2fd2a3f48e21f5c17
shubham <shubham.kumar.min21@itbhu.ac.in>
2024-03-05 02:36:43 +0530

    add monaco

--- apps/web/app/progress/[slug]/[slug1]/page.tsx
+++ apps/web/app/progress/[slug]/[slug1]/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 import React, { useEffect, useState, useRef } from "react";
-import Editor, { Monaco } from "@monaco-editor/react";
 const page = (props: any) => {
   const [userId, setUserId] = useState("");
   const [userName, setUserName] = useState("");
-  const [answers, setAnswers] = useState("");
   const slug = props.params.slug1;
   useEffect(() => {
     const loggedIn = localStorage.getItem("loggedIn");
@@ -16,43 +14,8 @@ const page = (props: any) => {
       setUserName(userName);
     }
   });
-  useEffect(() => {
-    const fetchSolvedQuestions = async () => {
-      try {
-        const response = await fetch(
-          `https://api.shubhamiitbhu.in/code/get-code-by-question-id-and-user-id`,
-          {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-              user_id: userId,
-            },
-            body: JSON.stringify({
-              userId: userId,
-              questionId: slug,
-            }),
-          },
-        );
-        const data = await response.json();
-        console.log(data[0].code);
-        setAnswers(data[0].code);
-      } catch (error) {
-        console.error("Error fetching solved questions:", error);
-      }
-    };
-
-    if (userId) {
-      fetchSolvedQuestions();
-    }
-  }, [userId, slug]);
   return (
     <>
-      {answers.length > 0 ? (
-        <>
-          <code>{answers}</code>
-        </>
-      ) : (
-        <>
           <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
             <embed
               src={`https://compiler.shubhamiitbhu.in/editor/${userName}/${slug}`}
@@ -61,27 +24,6 @@ const page = (props: any) => {
               style={{ backgroundColor: "wheat" }}
             />
           </div>
-          {/* <div style={{ width: "25%", height: "100vh", overflow: "hidden" }}>
-            <Editor
-              height="100%"
-              defaultLanguage="javascript"
-              defaultValue="// some code"
-              value={answers}
-              options={{
-                minimap: { enabled: false },
-                fontSize: 16,
-                wordWrap: "on",
-                scrollBeyondLastLine: false,
-                automaticLayout: true,
-              }}
-              onChange={(value: any) => {
-                setAnswers(value);
-              }}
-            />
-          </div> */}
-
-        </>
-      )}
     </>
   );
 };
