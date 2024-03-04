07d0e02c6570c2e489c8d4f44baf9669bb785f2f
shubham <shubham.kumar.min21@itbhu.ac.in>
2024-03-05 02:20:21 +0530

    add code compiler

--- apps/web/app/progress/[slug]/[slug1]/page.tsx
+++ apps/web/app/progress/[slug]/[slug1]/page.tsx
@@ -3,14 +3,17 @@ import React, { useEffect, useState, useRef } from "react";
 import Editor, { Monaco } from "@monaco-editor/react";
 const page = (props: any) => {
   const [userId, setUserId] = useState("");
+  const [userName, setUserName] = useState("");
   const [answers, setAnswers] = useState("");
   const slug = props.params.slug1;
   useEffect(() => {
     const loggedIn = localStorage.getItem("loggedIn");
     const userId = localStorage.getItem("userId");
-    if (loggedIn === "true" && userId) {
+    const userName = localStorage.getItem("userName");
+    if (loggedIn === "true" && userId && userName) {
       setUserId(userId);
       console.log(userId);
+      setUserName(userName);
     }
   });
   useEffect(() => {
@@ -52,7 +55,7 @@ const page = (props: any) => {
         <>
           <div style={{ width: "100%", height: "100vh", overflow: "hidden" }}>
             <embed
-              src="https://compiler.shubhamiitbhu.in/"
+              src={`https://compiler.shubhamiitbhu.in/editor/${userName}/${slug}`}
               width="100%"
               height="100%"
               style={{ backgroundColor: "wheat" }}
